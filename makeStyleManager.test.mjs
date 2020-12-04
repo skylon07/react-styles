5ca0f5eb60a5a1fc5c4c19c3ff3cc5cf06e5dcb5
Taylor Brown <taylor@davtar.org>
2020-12-03 19:01:24 -0500

    Initialize testing environment

---
+++ makeStyleManager.test.mjs
@@ -0,0 +1,44 @@
+import makeManager from './makeStyleManager.mjs'
+
+class MockComponent {
+    constructor() {
+        this.componentName = this.constructor.name
+        this.manager = makeManager.createComponentManager(this)
+    }
+
+    componentDidMount() {
+        this.manager.initStyle()
+        this.manager.updateDynamicStyles()
+    }
+
+    componentDidUpdate() {
+        this.manager.updateDynamicStyles()
+    }
+
+    makeStyle(when) {
+        throw new Error("Not implemented")
+    }
+}
+
+class FirstTest extends MockComponent {
+    makeStyle(when) {
+        return {
+            color: "white",
+            backgroundColor: "red",
+
+            '@keyframes FirstTest-Animation': {
+                from: {
+                    opacity: 0,
+                },
+
+                60: {
+                    opacity: 1,
+                }
+            }
+        }
+    }
+}
+
+const test = new FirstTest()
+test.componentDidMount()
+console.log(test.manager._getMemory().staticStyle.innerHTML)
