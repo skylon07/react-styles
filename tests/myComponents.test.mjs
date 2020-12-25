b9834fa58dc911d5ee6c4169b776dda83678efb4
Taylor Brown <taylor@davtar.org>
2020-12-25 15:29:51 -0500

    Add a file to test mocked React components

---
+++ tests/myComponents.test.mjs
@@ -0,0 +1,9 @@
+// NOTE: you will need to mock React in /reactStyles.mjs
+
+import ReactStyles from '../reactStyles.mjs'
+
+class MyComp extends ReactStyles.Component {
+
+}
+
+console.log(new MyComp().componentName)
